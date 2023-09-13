08c116fc5ff7c3646857f1721ac05bff63d3cae5
jamyth.luk <jamyth.luk@okg.com>
2023-09-13 14:27:56 +0800

    [FEATURE]: zsh: added fuzzy autocompletion

--- zsh/symlink.sh
+++ zsh/symlink.sh
@@ -1,16 +1,17 @@
-dir="$(dirname "${BASH_SOURCE[0]}")"
-
-unlink() {
-    if [[ -L "$HOME/$1" ]]; then
-        echo "symlink found for $1"
-    else
-        echo "$1 is not a symlink or cannot be found in $HOME"
-    fi
-}
-
-for file in $dir/.*; do
-    filename="$(basename "$file")"
-    if [[ "$filename" != "." && "$filename" != ".." ]]; then
-        unlink $filename
-    fi
-done
+# TODO/Jamyth, Update this file to symlink .zshrc & zsh/
+# dir="$(dirname "${BASH_SOURCE[0]}")"
+#
+# unlink() {
+#     if [[ -L "$HOME/$1" ]]; then
+#         echo "symlink found for $1"
+#     else
+#         echo "$1 is not a symlink or cannot be found in $HOME"
+#     fi
+# }
+#
+# for file in $dir/.*; do
+#     filename="$(basename "$file")"
+#     if [[ "$filename" != "." && "$filename" != ".." ]]; then
+#         unlink $filename
+#     fi
+# done
