792dc5ffdc4a175276a85cd1077656673b0dc922
jamyth.luk <jamyth.luk@okg.com>
2024-08-08 14:12:24 +0800

    feat: nvim2 - added buffer management

---
+++ nvim2/lua/jamyth/core/auto_cmd.lua
@@ -0,0 +1,10 @@
+vim.g.max_opened_buffers = 20
+
+local function manage_buffer_queue()
+	local buffers = vim.fn.getbufinfo({ buflisted = 1 })
+	if #buffers > vim.g.max_opened_buffers then
+		vim.cmd("bd " .. buffers[1].bufnr)
+	end
+end
+
+vim.api.nvim_create_autocmd("BufEnter", { callback = manage_buffer_queue })
--- nvim2/lua/jamyth/core/init.lua
+++ nvim2/lua/jamyth/core/init.lua
@@ -1,3 +1,4 @@
+require("jamyth.core.auto_cmd")
 require("jamyth.core.options")
 require("jamyth.core.keymaps")
 require("jamyth.core.dacs_options")
