91bddb1b9dc73b107b284034d3ac09624f58ef3e
Jamyth <jamyth0712@gmail.com>
2025-01-31 04:01:14 +0800

    refactor: nvim-2025 - fix path issue

--- nvim-2025/init.lua
+++ nvim-2025/init.lua
@@ -1,8 +1,10 @@
 local Util = require("core.utils")
 
 local profile = os.getenv("NVIM_PROFILE") or "default"
-local profile_path = Util.join_system_path("lua", profile)
+local config_directory = vim.fn.stdpath("config")
+local profile_path = Util.join_system_path(config_directory, "lua", profile)
 
+-- Make sure the custom direct exists before loading
 if Util.is_directory(profile_path) and profile ~= "core" then
 	require(profile)
 else
