35ffe91a352a61b07519310db11fddb064fd457a
Jamyth <jamyth0712@gmail.com>
2025-02-08 23:44:23 +0800

    feat: nvim-2025 - added neogit and grouped ui related plugins

--- nvim-2025/lua/playground/init.lua
+++ nvim-2025/lua/playground/init.lua
@@ -2,4 +2,5 @@ require("playground.main")
 require("core.lazy_nvim").setup({
 	{ import = "playground.plugins" },
 	{ import = "playground.plugins.basic" },
+	{ import = "playground.plugins.basic.ui" },
 })
---
+++ nvim-2025/lua/playground/plugins/neogit.lua
@@ -0,0 +1,17 @@
+return {
+    "NeogitOrg/neogit",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "sindrets/diffview.nvim",
+        "nvim-telescope/telescope.nvim"
+    },
+    config = function()
+        local neogit = require("neogit")
+        local keymap = vim.keymap
+
+        neogit.setup()
+
+        -- keymaps
+        keymap.set("n", "<leader>lg", function() neogit.open({ kind = "floating" }) end, { desc = "Open Neogit" })
+    end
+}
